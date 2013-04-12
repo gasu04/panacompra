992313de8a7151da2935c43e9bb87ecd27cab666
Baconator507 <ibarria0@gmail.com>
2013-04-12 02:05:35 -0400

    regex match juicy data

--- Worker.py
+++ Worker.py
@@ -1,7 +1,9 @@
 import threading
 import httplib, urllib
+import re
 from Compra import Compra
 from time import sleep
+from bs4 import BeautifulSoup
 
 class WorkThread(threading.Thread):
   """
@@ -14,6 +16,10 @@ class WorkThread(threading.Thread):
     self.compra_urls = queue
     self.compras = out_queue
     self.scrapers = scrapers
+    self.precio_regex = re.compile("(?:Precio.*?>.*?>[^0-9]*)([0-9]*\.[0-9][0-9]*)") 
+    self.acto_regex = re.compile("(?:ero de Acto:</td><td class=\"formEjemplos\">)([^<]*)")
+    self.entidad_regex = re.compile("(?:Entidad:</td><td class=\"formEjemplos\">)([^<]*)") 
+    self.proponente_regex = re.compile("(?:Proponente.*\n.*\n.*?Ejemplos\">)([^<]*)",re.MULTILINE)
 
   def run(self):
     while True:
@@ -33,7 +39,11 @@ class WorkThread(threading.Thread):
     self.compras.put(Compra(url,html,self.parse_compra_html(html))) #create and store Compra object
 
   def parse_compra_html(self,html):
-    return [1]
+    precio = self.precio_regex.findall(html)
+    acto = self.acto_regex.findall(html)
+    entidad = self.entidad_regex.findall(html)
+    proponente = self.proponente_regex.findall(html)
+    return { 'entidad' : entidad, 'acto': acto, 'precio' : precio, 'proponente' : proponente}
 
   def get_compra_html(self,url):
     connection = httplib.HTTPConnection("www.panamacompra.gob.pa", "80")
