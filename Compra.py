11e3e6dc4c4205a47a6750cff3546e7a6012cbf0
Baconator507 <ibarria0@gmail.com>
2013-05-09 17:28:48 -0400

    category translation

--- Compra.py
+++ Compra.py
@@ -4,9 +4,16 @@ class Compra():
     self.category = category
     self.html = html
     self.data = data
+    self.categories = {50: "Alimentos, Bebidas y Tabaco",15:"Combustibles, Aditivos para combustibles, Lubricantes y Materiales Anticorrosivos",31:'Componentes y Suministros de Fabricación', 30:'Componentes y Suministros de Fabricación, Estructuras, Obras y Construcciones',25:'Componentes y Suministros Electrónicos',35:'Vehículos Comerciales, Militares y Particulares, Accesorios y Componentes'  }
+
+  def translate_category(self,category_number):
+    if category_number in self.categories:
+      return self.categories[category_number]
+    return category_number
 
   def __str__(self):
     return str(self.data)
 
+
   def to_json(self):
-    return {'url': self.url, 'category': self.category, 'data': self.data}
+    return {'url': self.url, 'category': self.translate_category(self.category), 'data': self.data}
