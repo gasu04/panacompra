9b3496a0bbb691a1bc88e0d03e12d96ad4d8faf0
Baconator507 <ibarria0@gmail.com>
2013-06-13 10:56:54 -0400

    fields for sqlalchemy

--- Compra.py
+++ Compra.py
@@ -1,4 +1,16 @@
-class Compra():
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import *
+
+Base = declarative_base()
+
+class Compra(Base):
+  __tablename__ = 'compras'
+  
+  id = Column(Integer, Sequence('compra_id_seq'), primary_key=True)
+  url = Column(String(100))
+  category = Column(String(50))
+  html = Column(BLOB)
+
   def __init__(self,url,category,html,data):
     self.url = url
     self.category = category
@@ -14,6 +26,8 @@ class Compra():
   def __str__(self):
     return str(self.data)
 
-
   def to_json(self):
     return {'url': self.url, 'category': self.translate_category(self.category), 'data': self.data}
+
+  def to_insert(self):
+    return [self.data['entidad'],self.translate_category(self.category),self.data['proponente'],self.data['precio'],self.data['fecha'],self.data['acto'],self.data['descripcion']]
