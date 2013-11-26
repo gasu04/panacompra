6c571f0cbc8756f820982fb34cc2985a92f88379
Baconator507 <ibarria0@gmail.com>
2013-11-25 22:24:40 -0500

    error page and logging

--- modules/parser.py
+++ modules/parser.py
@@ -2,22 +2,26 @@ import re
 from decimal import Decimal
 from datetime import datetime
 from bs4 import BeautifulSoup,SoupStrainer
+import logging
+
+logger = logging.getLogger('Parser')
 
 def is_error_page(soup):
     error = soup.find(text=re.compile("unhandled exception"))
-    if error or len(soup) == 0:
+    error2 = soup.find(text=re.compile("internal server error"))
+    if error or error2 or len(soup) == 0:
         return True
     return False
 
 def parse_html(compra,methods):
-    soup = BeautifulSoup(compra.html,'html.parser',parse_only=SoupStrainer('tr'), from_encoding='ISO-8859-1')
+    soup = BeautifulSoup(compra.html,'html.parser',parse_only=SoupStrainer('tr'))
     if not is_error_page(soup):
         for name,method in methods.items():
             try:
                 val = method(soup)
                 setattr(compra,name,val)
             except Exception as e:
-                print(e)
+                logger.error('error getting %s from %s', name, compra.url)
     compra.parsed = True
     return compra 
 
@@ -26,8 +30,9 @@ def parse_date(date):
     date = date.replace('.','').upper()
     date = datetime.strptime(date,"%d-%m-%Y %I:%M %p") 
   except Exception as e:
-    print(e)
-    date= None
+    logger.error("%s", e)
+    logger.error('error parsing date %s', date)
+    date = None
   return date
 
 def parse_precio(precio):
@@ -58,12 +63,12 @@ def extract_precio(soup):
     try:
         precio = soup.find('td',text='Precio Referencia:').find_next_sibling('td').string
     except AttributeError:
-        precio = soup.find('td',text=re.compile('Monto de la Contrataci.')).find_next_sibling('td').string 
+        precio = soup.find('td',text='Monto de la Contratación:').find_next_sibling('td').string 
     finally:
         return parse_precio(precio)
     
 def extract_description(soup):
-    a = sanitize(soup.find('td',text=re.compile('Descripci.')).find_next_sibling('td').string)
+    a = sanitize(soup.find('td',text='Descripción:').find_next_sibling('td').string)
     return a
 
 def extract_compra_type(soup):
@@ -79,15 +84,15 @@ def extract_objeto(soup):
     return sanitize(soup.find('td',text='Objeto Contractual:').find_next_sibling('td').string)
     
 def extract_modalidad(soup):
-    modalidad = soup.find('td',text=re.compile('Modalidad.'))
+    modalidad = soup.find('td',text='Modalidad de adjudicación:')
     if modalidad is not None:
         return sanitize(modalidad.find_next_sibling('td').string)
     return None
     
 def extract_provincia(soup):
-    provincia = soup.find('td',text='Provincia de Entrega:')
+    provincia = soup.find(text='Provincia de Entrega:')
     if provincia is not None:
-        return sanitize(provincia.find_next_sibling('td').string)
+        return sanitize(provincia.parent.find_next_sibling('td').string)
     return None
     
 def extract_correo_contacto(soup):
@@ -100,10 +105,10 @@ def extract_telefono_contacto(soup):
     return sanitize(soup.find('td',text='Datos de Contacto').parent.find_next_sibling('tr').find_next_sibling('tr').find_next_sibling('tr').find('td').find_next_sibling('td').string[:14])
 
 def extract_fecha(soup):
-    return parse_date(soup.find('td',text=re.compile('Fecha de Pub.*')).find_next_sibling('td').string)
+    return parse_date(soup.find('td',text='Fecha de Publicación:').find_next_sibling('td').string)
     
 def extract_acto(soup):
-    return sanitize(soup.find('td',text=re.compile('.Acto.')).find_next_sibling('td').string)
+    return sanitize(soup.find(text='Número de Acto:').parent.find_next_sibling('td').string)
 
 def extract_entidad(soup):
     return sanitize(soup.find('td',text='Entidad:').find_next_sibling('td').string)
